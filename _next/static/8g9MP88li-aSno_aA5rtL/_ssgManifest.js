a043ed4292b5709191e07fa3b3efa4b3d5cd2e5d
Khaled Gomaa <khaled.gomaa.90@gmail.com>
2026-05-03 22:31:21 +0300

    deploy: move static files to root for GitHub Pages

---
+++ _next/static/8g9MP88li-aSno_aA5rtL/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fadmin\u002Ftours\u002F[id]\u002Fedit","\u002Fadmin\u002Ftours\u002F[id]\u002Fslots","\u002Ftours\u002F[id]","\u002Fvoucher\u002F[code]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
